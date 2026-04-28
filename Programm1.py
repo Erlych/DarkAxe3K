7a1a438310caa7733f65e910a30a16597ba06956
Erlych <her49252@gmail.com>
2026-04-28 20:55:46 +0300

    1

--- Programm1.py
+++ Programm1.py
@@ -9,13 +9,16 @@ from pyexpat.errors import messages
 #Ввод библиотек
 
 #Ввод переменных
-a = 0  #Количество очков
+a = 99999999 #Количество очков
 b = 0  #На сколько прибавляется(автоклик)
 c = 100#Цена обновления автоклика
 d = 0  #Количество обновлений автоклика
 e = 1  #На сколько прибавляется(клик)
 f = 50 #Цена за обновление клика
 g = 1  #Количество обновлений клика
+h = 10  #Время автоклика
+i = 500#Цена обновления времени
+j = 0  #Количество обновлений времени
 
 
 class Options(Toplevel):
@@ -45,10 +48,10 @@ class Options(Toplevel):
 
 
 def num():  #Функция по автоматическому прибавлению очков
-    global a, d
+    global a, d, h
     while True:
         if d >= 1:
-            time.sleep(1)
+            time.sleep(h/10)
             a += b #Прибаление очков
             label1["text"] = a #Изменение текста на счётчике
 def exit():  #Функция для выхода
@@ -61,13 +64,13 @@ def update():#Функция отвечающая за обновления
         b += 1 #Прибавление к ежесекундной прибавке очков
         d += 1 #Общее кол-во обновление +1
         c *= 2 #Увеличение цены
-        Button1["text"] = f"Update - {c}" #Изменение текста на кнопке обновления
+        Button1["text"] = f"Update(autoclick) - {c}" #Изменение текста на кнопке обновления
         label1["text"] = a  # Изменение текста на счётчике
 def click(): #Функция отвечающая за клик
     global a, e
     a += e
     label1["text"] = a
-    label2["text"] = f"AutoClick - {d} ( +{b} ); Click - {g}( +{e} )" #Обновление информации о доходе
+    label2["text"] = f"AutoClick - {d} ( +{b}; {h/10}sec ); Click - {g}( +{e} )" #Обновление информации о доходе
 def updk():
     global a, e, f, g
     if a >= f:
@@ -77,6 +80,19 @@ def updk():
         g += 1
         Button3["text"] = f"Update(click) - {f}"
         label1["text"] = a  # Изменение текста на счётчике
+def updt():
+    global a, h, i, j, b
+    if a >= i:
+        a -= i
+        i *= 2
+        if h <= 1:
+            b += 1
+            h /= 10
+        elif h > 1:
+            h -= 1
+        j += 1
+        label1["text"] = a
+        Button4["text"] = f"Update time autoclick - {i}"
 def opt():
     Options()
     print("Открытие окна настроек")
@@ -99,13 +115,15 @@ Button4 = ttk.Button(text="Options", command=opt)
 Button4.pack(anchor=NE)#Добавление кнопки настроек
 label1 = ttk.Label(text=a)
 label1.place(relx=.5, rely=.5, anchor="center")#Добавление счётчика очков(+расположение)
-Button1 = ttk.Button(text=f"Buy autoclick!!( {c} )", command=update)
+Button3 = ttk.Button(text=f"Update click! - {f}", command=updk)
+Button3.pack(anchor=NW)#Добавление кнопки улучшения клика
+Button1 = ttk.Button(text=f"Buy autoclick!! - {c}", command=update)
 Button1.pack(anchor=NW)#Добавление кнопки улучшения автоклика
+Button4 = ttk.Button(text=f"Update time autoclick - {i}", command=updt)
+Button4. pack(anchor=NW)#Добавление кнопки улучшения времени автоклика
 Button2 = ttk.Button(text="Click", command=click)
 Button2.place(relx=.5, rely=.6, anchor="center")#Добавление кнопки клика
-Button3 = ttk.Button(text=f"Update click!( {f} )", command=updk)
-Button3.pack(anchor=NW)#Добавление кнопки улучшения клика
-label2 = ttk.Label(text=f"AutoClick - {d} ( +{b} ); Click - {g}( +{e} )")
+label2 = ttk.Label(text=f"AutoClick - {d} ( +{b}; {h/10}sec ); Click - {g}( +{e} )")
 label2.place(relx=.5, rely=.55, anchor="center")#Добавление информации о доходе
 
 root.mainloop()
